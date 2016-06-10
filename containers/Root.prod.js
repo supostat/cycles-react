e873ff852fc3434d142d12f4582a979ae654c02e
supostat <ipugachev84@gmail.com>
2016-06-10 12:16:00 +0600

    Initial commit

---
+++ containers/Root.prod.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import TodoApp from './TodoApp';
+
+export default class Root extends Component {
+  render() {
+    console.log(this.props);
+    const { store } = this.props;
+    return (
+      <Provider store={store}>
+        <TodoApp />
+      </Provider>
+    );
+  }
+}
